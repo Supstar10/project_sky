ba3fbe7557fa459fd7218423181a2dfdba266f0b
starcev daniil <stdanmark@mail.ru>
2024-09-18 19:27:57 +0300

    20.2 done

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator
+
+from catalog.forms import ProductForm
+from catalog.models import Product
 
 
 def home(request):
-    return render(request, 'project_sky/home.html')
+    products = Product.objects.all()
+    paginator = Paginator(products, 4)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'project_sky/product.html', {"page_obj": page_obj})
 
 
 def contacts(request):
@@ -12,3 +20,19 @@ def contacts(request):
         message = request.POST.get('message')
         print(f"{name} ({phone}): {message}")
     return render(request, 'project_sky/contacts.html')
+
+
+def info_product(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, "project_sky/info_product.html", {"product": product})
+
+def create_product(request):
+    request.method = 'POST'
+    form = ProductForm(request.POST, request.FILES)
+    if form.is_valid():
+        form.save()
+        return redirect("catalog:home")
+    return render(request, "project_sky/create_products.html", {"form": form})
+def buy_product(request):
+    buy = Product.objects.all()
+    return render(request, "project_sky/buy_product.html", {"buy": buy})
