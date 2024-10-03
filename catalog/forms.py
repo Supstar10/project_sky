e2a28188466bfb4516edd7f47f7cce1259153c9c
starcev daniil <stdanmark@mail.ru>
2024-10-03 13:29:44 +0300

    done

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,7 +1,17 @@
 from django import forms
 from .models import Product
+from .models import Blog
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ["name", "price", "category", "preview", "description"]
+        fields = ["name", "price", "category", "preview", "description", 'manufactured_at',]
+
+
+class BlogForm(forms.ModelForm):
+    class Meta:
+        model = Blog
+        fields = ['heading', 'content', 'slug', 'preview', 'sing_of_publication']
+
+
+
--- catalog/views.py
+++ catalog/views.py
@@ -1,38 +1,69 @@
+from django.core.mail import send_mail
 from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator
+from django.urls import reverse_lazy, reverse
+from django.utils.text import slugify
+from django.views.generic import ListView, TemplateView, CreateView, DetailView, UpdateView, DeleteView
 
 from catalog.forms import ProductForm
-from catalog.models import Product
+from catalog.models import Product, Contact, Blog
+from config import settings
 
 
-def home(request):
-    products = Product.objects.all()
-    paginator = Paginator(products, 4)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
-    return render(request, 'project_sky/product.html', {"page_obj": page_obj})
+class ProductListView(ListView):
+    model = Product
+    paginate_by = 4
+
+class ContactsListView(TemplateView):
+    template_name = "catalog/contact_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["object_list"] = Contact.objects.all()
+        return context
+
+class ProductDetailView(DetailView):
+    model = Product
 
 
-def contacts(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        message = request.POST.get('message')
-        print(f"{name} ({phone}): {message}")
-    return render(request, 'project_sky/contacts.html')
+class CreateProductListView(CreateView):
+    model = Product
+    fields = ("name", "price", "category", "preview", "description")
+    success_url = reverse_lazy('catalog:home')
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    fields = ("name", "price", "category", "preview", "description")
+    success_url = reverse_lazy('catalog:home')
+class BlogListView(ListView):
+    model = Blog
+    #queryset = Blog.objects.filter(is_published=True).order_by('-created_at')
+
+class BlogDetailView(DetailView):
+    model = Blog
 
 
-def info_product(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    return render(request, "project_sky/info_product.html", {"product": product})
+class BlogCreateView(CreateView):
+    model = Blog
+    fields = ("heading", "content", "preview", "count_views")
+    success_url = reverse_lazy('catalog:blog_list')
 
-def create_product(request):
-    request.method = 'POST'
-    form = ProductForm(request.POST, request.FILES)
-    if form.is_valid():
-        form.save()
-        return redirect("catalog:home")
-    return render(request, "project_sky/create_products.html", {"form": form})
-def buy_product(request):
-    buy = Product.objects.all()
-    return render(request, "project_sky/buy_product.html", {"buy": buy})
+    def form_valid(self, form):
+        if form.is_valid():
+            obj = form.save()
+            obj.slug = slugify(obj.title)
+            obj.save()
+        return super().form_valid(form)
+
+class BlogUpdateView(UpdateView):
+    model = Blog
+    fields = ("heading", "slug", "content", "preview", "sing_of_publication")
+    success_url = reverse_lazy("catalog:blog_list")
+
+    def get_success_url(self):
+        return reverse('catalog:detail_blog', args=[self.kwargs.get('slug')])
+
+class BlogDeleteView(DeleteView):
+    model = Blog
+    success_url = reverse_lazy("catalog:blog_list")
+
